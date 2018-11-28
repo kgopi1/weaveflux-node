e1137827bada6e1fdfabe3e5ae979dad7b6544f2
Mahendra Hegde <hegde.mahendra@tcs.com>
2018-11-28 16:48:36 +0530

    changed node to accept args

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
-const message=`hello from nodeJs`;
+const message=process.argv[2];
 http.createServer(function (req, res) {
     res.write(message); //write a response to the client
     res.end(); //end the response
- 
 }).listen(8001);
