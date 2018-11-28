c34ee629c8817fe354f1bd7e73d1ee1a83548304
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2018-11-28 16:25:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const message=`hello from nodeJs`;
+const message=`hello from Fresco`;
 http.createServer(function (req, res) {
     res.write(message); //write a response to the client
     res.end(); //end the response
