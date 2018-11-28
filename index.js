fcf16e45fe3ddc050e52066c765d420c433d31ae
Mahendra Hegde <hegde.mahendra@tcs.com>
2018-11-28 17:44:32 +0530

    added env msg

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const message=process.argv[2];
+const message=process.env.msg;
 http.createServer(function (req, res) {
     res.write(message); //write a response to the client
     res.end(); //end the response
