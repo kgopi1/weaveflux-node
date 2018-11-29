f300ebf5a2dd16b580b5451d76b1bc0606118269
Mahendra Hegde <hegde.mahendra@tcs.com>
2018-11-29 09:15:41 +0530

    handled no env.msg

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const message=process.env.msg;
+const message=process.env.msg || "No environmental variable(msg)";
 http.createServer(function (req, res) {
     res.write(message); //write a response to the client
     res.end(); //end the response
